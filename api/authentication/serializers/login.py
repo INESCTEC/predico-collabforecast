d0b6d75faafa0efd427321923aa635852f30f10d
itsjrsa <jrsa2012@gmail.com>
2024-10-12 16:26:28 +0100

    Removed unnecessary personal information from JWT.

--- api/authentication/serializers/login.py
+++ api/authentication/serializers/login.py
@@ -10,10 +10,9 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
-        token['email'] = user.email
-        token['first_name'] = user.first_name
-        token['last_name'] = user.last_name
-
+        # token['email'] = user.email
+        # token['first_name'] = user.first_name
+        # token['last_name'] = user.last_name
         return token
 
     def validate(self, attrs):
