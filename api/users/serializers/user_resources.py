7819a037746f89aca736e2a4727cd4a4fe9f4cad
itsjrsa <jrsa2012@gmail.com>
2024-11-06 00:08:08 +0000

    Now also returns raw measurements metadata information on /user/resource endpoint. Updated examples accordingly.

--- api/users/serializers/user_resources.py
+++ api/users/serializers/user_resources.py
@@ -2,11 +2,38 @@ import datetime as dt
 
 from rest_framework import serializers
 
+from data.models.raw_data import RawData
+
 from ..util.validators import ResourceNameValidator
 from ..models.user_resources import UserResources
 
 
-class UserResourcesSerializer(serializers.ModelSerializer):
+class UserResourcesRetrieveSerializer(serializers.ModelSerializer):
+    measurements_n_samples = serializers.IntegerField(read_only=True)
+    measurements_start = serializers.DateTimeField(read_only=True)
+    measurements_end = serializers.DateTimeField(read_only=True)
+    measurements_last_update = serializers.DateTimeField(read_only=True)
+
+    class Meta:
+        model = UserResources
+        exclude = []
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        count_measurement = RawData.objects.filter(resource_id=instance.id).count()
+        first_measurement = RawData.objects.filter(resource_id=instance.id).order_by('datetime').first()
+        last_measurement = RawData.objects.filter(resource_id=instance.id).order_by('datetime').last()
+        last_update = RawData.objects.filter(resource_id=instance.id).order_by('registered_at').last()
+        representation["measurements_metadata"] = {
+            "n_samples": count_measurement,
+            "start_datetime": first_measurement.datetime if first_measurement else None,
+            "end_datetime": last_measurement.datetime if last_measurement else None,
+            "last_update": last_update.registered_at if last_update else None,
+        }
+        return representation
+
+
+class UserResourcesUpdateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UserResources
@@ -39,5 +66,5 @@ class UserResourcesCreateSerializer(serializers.ModelSerializer):
     Note: This is used just to avoid having the "user" field as example
     in the Swagger documentation (for POST requests)
     """
-    class Meta(UserResourcesSerializer.Meta):
+    class Meta(UserResourcesUpdateSerializer.Meta):
         exclude = ['user']
